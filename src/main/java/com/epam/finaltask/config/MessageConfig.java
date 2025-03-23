131ead157cb028a4163dbded19f5d17f7edb3d84
ArturGoz <arturrm4@gmail.com>
2025-03-23 12:56:47 +0200

    main page localized added config for messages

---
+++ src/main/java/com/epam/finaltask/config/MessageConfig.java
@@ -0,0 +1,17 @@
+package com.epam.finaltask.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages"); // Path to messages.properties
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
