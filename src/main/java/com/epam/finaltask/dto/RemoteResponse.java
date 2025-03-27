f7daf9a1b4c8c4fed5ad0d5073b1de6cab75010c
ArturGoz <arturrm4@gmail.com>
2025-03-27 18:57:27 +0200

    logging fix

--- src/main/java/com/epam/finaltask/dto/RemoteResponse.java
+++ src/main/java/com/epam/finaltask/dto/RemoteResponse.java
@@ -23,8 +23,18 @@ public class RemoteResponse {
         response.setStatusCode(statusCode);
         response.setStatusMessage(statusMessage);
         response.setResults(additionalElements);
-        log.info("Response details: succeeded={}, statusCode={}, statusMessage={}, results={}",
-                succeeded, statusCode, statusMessage, additionalElements);
+
+
+        String logMessage = String.format(
+                "Response details: succeeded=%s, statusCode=%s, statusMessage=%s, results=%s",
+                succeeded, statusCode, statusMessage, additionalElements
+        );
+
+        if (succeeded) {
+            log.info(logMessage);
+        } else {
+            log.error(logMessage);
+        }
         return response;
     }
 }
